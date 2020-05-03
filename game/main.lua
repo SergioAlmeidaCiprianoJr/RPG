a6042a35a9f540404a30bf00022a2e7d235ab88d
SergioAlmeidaCiprianoJr <sergiosacj@hotmail.com.br>
2020-05-03 14:41:40 -0300

    Adding new protagonist pixel art

--- game/main.lua
+++ game/main.lua
@@ -7,8 +7,8 @@ anim8 = require('/src/libraries/anim8')
 local background, p
 
 function love.load()
-	background = love.graphics.newImage('pixel_art/scenario/teste.png')
 	player = Player:new(82,200,'Sergio')
+	background = love.graphics.newImage('pixel_art/scenario/cidade.png')
 end
 
 function love.update(dt)
@@ -17,5 +17,5 @@ end
 
 function love.draw()
 	love.graphics.draw(background, 0, 0)
-	player:draw(image, player.x, player.y)
+	player:draw()
 end
--- game/src/player.lua
+++ game/src/player.lua
@@ -2,7 +2,7 @@ Player = {}
 Player.__index = Player
 
 -- Const
-local WALK_SPEED = 3
+local WALK_SPEED = 7
 local RUN_SPEED = 0
 local MAX_LIFE = 0
 
@@ -18,44 +18,37 @@ function Player:new(x,y,name)
 	self.x, self.y = x, y
 
 	-- Image
-	self.imgFront = love.graphics.newImage('pixel_art/protagonist/player_front.png')
-	self.imgBack = love.graphics.newImage('pixel_art/protagonist/player_back.png')
-	self.imgLeft = love.graphics.newImage('pixel_art/protagonist/player_left_side.png')
-	self.imgRight = love.graphics.newImage('pixel_art/protagonist/player_right_side.png')
+	self.imgFront = love.graphics.newImage('pixel_art/protagonist/demon_warrior_grid.png')
+	self.imgBack = love.graphics.newImage('pixel_art/protagonist/demon_warrior_grid_iv.png')
+	self.imgPrevious = self.imgFront
 
 	-- Grid
-	self.gridFront = anim8.newGrid(35, 64, self.imgFront:getWidth(), self.imgFront:getHeight(), 0, 0, 2)
-	self.gridBack = anim8.newGrid(35, 64, self.imgBack:getWidth(), self.imgBack:getHeight(), 0, 0, 2)
-	self.gridLeft = anim8.newGrid(35, 64, self.imgLeft:getWidth(), self.imgLeft:getHeight(), 0, 0, 2)
-	self.gridRight = anim8.newGrid(35, 64, self.imgRight:getWidth(), self.imgRight:getHeight(), 0, 0, 2)
+	self.grid = anim8.newGrid(125, 148, self.imgFront:getWidth(), self.imgFront:getHeight(), 0, 0, 8)
 
 	-- Animation
-	self.animateFront = anim8.newAnimation(self.gridFront('1-5',1), 0.1)
-	self.animateBack = anim8.newAnimation(self.gridBack('1-5',1), 0.1)
-	self.animateLeft = anim8.newAnimation(self.gridLeft('1-5',1), 0.1)
-	self.animateRight = anim8.newAnimation(self.gridRight('1-5',1), 0.1)
+	self.animate = anim8.newAnimation(self.grid('1-3',1), 0.1)
 
 	return self
 end
 
 function Player:update(dt)
-	self.animateFront:update(dt)
-	self.animateBack:update(dt)
-	self.animateLeft:update(dt)
-	self.animateRight:update(dt)
-
+	if self.direction ~= nil then
+		self.animate:update(dt)
+	else
+		self.animate:gotoFrame(1)
+	end	
 	self:move()
 end
 
 function Player:draw()
-	if self.direction == 'N' then
-		self.animateBack:draw(self.imgBack, self.x, self.y)
-	elseif self.direction == 'S' then
-		self.animateFront:draw(self.imgFront, self.x, self.y)
+	if self.direction == 'O' then
+		self.animate:draw(self.imgBack, self.x, self.y)
+		self.imgPrevious = self.imgBack
 	elseif self.direction == 'L' then
-		self.animateRight:draw(self.imgRight, self.x, self.y)
-	elseif self.direction == 'O' then
-		self.animateLeft:draw(self.imgLeft, self.x, self.y)
+		self.animate:draw(self.imgFront, self.x, self.y)
+		self.imgPrevious = self.imgFront
+	else
+		self.animate:draw(self.imgPrevious, self.x, self.y)
 	end
 end
 
@@ -63,17 +56,16 @@ function Player:move()
 	if love.keyboard.isDown('w') then
 		self.y = self.y - WALK_SPEED
 		self.direction = 'N'
-	end
-	if love.keyboard.isDown('s') then
+	elseif love.keyboard.isDown('s') then
 		self.y = self.y + WALK_SPEED
 		self.direction = 'S'
-	end
-	if love.keyboard.isDown('d') then
+	elseif love.keyboard.isDown('d') then
 		self.x = self.x + WALK_SPEED
 		self.direction = 'L'
-	end
-	if love.keyboard.isDown('a') then
+	elseif love.keyboard.isDown('a') then
 		self.x = self.x - WALK_SPEED
 		self.direction = 'O'
+	else
+		self.direction = nil
 	end  
 end
