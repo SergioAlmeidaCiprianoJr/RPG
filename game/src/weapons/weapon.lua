5597183d68e2cddaf23d656af829a0760dacc3a1
SergioAlmeidaCiprianoJr <sergiosacj@hotmail.com.br>
2020-05-06 19:23:59 -0300

    Adding weapon animation

--- game/src/weapons/weapon.lua
+++ game/src/weapons/weapon.lua
@@ -1,6 +1,9 @@
 Weapon = {}
 Weapon.__index = Weapon
 
+-- CONST
+local FRAME = 25
+
 function Weapon:new(x, y, name, img, w, h)
     self = setmetatable({}, Weapon)
 
@@ -10,18 +13,30 @@ function Weapon:new(x, y, name, img, w, h)
 end
 
 function Weapon:update(dt)
+    self:attack()
     self.animate:update(dt)
 end
 
 function Weapon:draw()
-    local x, y = player:position()
-    self.animate:draw(self.img, x+player.width, y)
+    if self.count > 0 then
+        local x, y = player:position()
+        self.animate:draw(self.img, x+player.width, y-20)
+        self.count = self.count - 1
+    end
 end
 
 function Weapon:change(x, y, name, img, w, h)
     self.name = name
+    self.count = 0
     self.img = love.graphics.newImage(img)
     self.grid = anim8.newGrid(w, h, self.img:getWidth(), self.img:getHeight(), 0, 0, 0)
     self.animate = anim8.newAnimation(self.grid('1-4', 1), 0.1)
     self.collider = world:newRectangleCollider(x, y, w, h)
+end
+
+function Weapon:attack()
+    if love.keyboard.isDown('k') then
+        self.count = FRAME
+        self.animate:gotoFrame(1)
+    end
 end
