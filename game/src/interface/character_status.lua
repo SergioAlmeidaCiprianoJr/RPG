070c42a4c87025db2dfc67d6ba37a3925f445c4d
SergioAlmeidaCiprianoJr <sergiosacj@hotmail.com.br>
2020-05-10 20:10:47 -0300

    Making character status become a class

--- game/src/interface/character_status.lua
+++ game/src/interface/character_status.lua
@@ -1,26 +1,31 @@
-local imgLife, imgLifeDamaged, imgLifeEmpty
+Status = {}
+Status.__index = Status
 
-function loadLife(life, lifeDamaged, lifeEmpty)
-    imgLife = love.graphics.newImage(life)
-    imgLifeDamaged = love.graphics.newImage(lifeDamaged)
-    imgLifeEmpty = love.graphics.newImage(lifeEmpty)
+function Status:new(life, lifeDamaged, lifeEmpty)
+    self = setmetatable({}, Status)
+
+    self.imgLife = love.graphics.newImage(life)
+    self.imgLifeDamaged = love.graphics.newImage(lifeDamaged)
+    self.imgLifeEmpty = love.graphics.newImage(lifeEmpty)
+
+    return self
 end
 
-function drawLife(life, lifeDamaged, lifeEmpty, imgSize, posX, posY)
+function Status:drawLife(life, lifeDamaged, lifeEmpty, imgSize, posX, posY)
     local i
 
     for i = life, 1, -1 do
-        love.graphics.draw(imgLife, posX, posY)
+        love.graphics.draw(self.imgLife, posX, posY)
         posX = posX+75
     end
 
     for i = lifeDamaged, 1, -1 do
-        love.graphics.draw(imgLifeDamaged, posX, posY)
+        love.graphics.draw(self.imgLifeDamaged, posX, posY)
         posX = posX+75
     end
 
     for i = lifeEmpty, 1, -1 do
-        love.graphics.draw(imgLifeEmpty, posX, posY)
+        love.graphics.draw(self.imgLifeEmpty, posX, posY)
         posX = posX+75
     end
 end
--- game/src/resources/manage_resources.lua
+++ game/src/resources/manage_resources.lua
@@ -21,36 +21,40 @@ end
 function drawResources()
 	camera:set()
         mapBattlefield:draw()
-        player:draw()
-        drawLife(player.life, player.lifeDamaged, player.lifeEmpty, 75, camera.x, camera.y)
+        drawPlayer()
         weapon:draw()
         world:draw()
-	camera:unset()
-end
-
-function loadCollisionClass()
-    --world:addCollisionClass('Ghost', {ignores = {'Solid'}})
-    world:addCollisionClass('Player')
-    world:addCollisionClass('Enemy')
-end
-
-function loadPlayer()
-    local playerImg = {right = '/pixel_art/protagonist/demon_warrior_grid.png', left = '/pixel_art/protagonist/demon_warrior_grid_iv.png'}
-    player = Player:new(500, 500, 'Sergio', playerImg.right, playerImg.left)
-    loadLife('/pixel_art/assets/hearth.png', '/pixel_art/assets/hearth_damaged.png', '/pixel_art/assets/hearth_empty.png')
-end
-
-function loadWeapons()
-    weapon = Weapon:new(500, 500, 'sword', '/pixel_art/weapons/katana_animation_grid_iv.png', '/pixel_art/weapons/katana_animation_grid.png', 31*4, 40*4)
-end
-
-function loadBattlefield()
-    local collision = {
-        l1 = {32,   159,  2008, 159},       -- top
-        l2 = {32,   1888, 2008, 1888},      -- bottom
-        l3 = {32,   159,  32,   1888},      -- left
-        l4 = {2008, 159,  2008, 1888}       -- right
-    }
-
-    mapBattlefield = Map:new('battefield', '/pixel_art/scenario/battlefield.png', collision)
+        camera:unset()
+    end
+    
+    function loadCollisionClass()
+        --world:addCollisionClass('Ghost', {ignores = {'Solid'}})
+        world:addCollisionClass('Player')
+        world:addCollisionClass('Enemy')
+    end
+    
+    function loadPlayer()
+        local playerImg = {right = '/pixel_art/protagonist/demon_warrior_grid.png', left = '/pixel_art/protagonist/demon_warrior_grid_iv.png'}
+        player = Player:new(500, 500, 'Sergio', playerImg.right, playerImg.left)
+        playerStatus = Status:new('/pixel_art/assets/hearth.png', '/pixel_art/assets/hearth_damaged.png', '/pixel_art/assets/hearth_empty.png')
+    end
+    
+    function loadWeapons()
+        weapon = Weapon:new(500, 500, 'sword', '/pixel_art/weapons/katana_animation_grid_iv.png', '/pixel_art/weapons/katana_animation_grid.png', 31*4, 40*4)
+    end
+    
+    function loadBattlefield()
+        local collision = {
+            l1 = {32,   159,  2008, 159},       -- top
+            l2 = {32,   1888, 2008, 1888},      -- bottom
+            l3 = {32,   159,  32,   1888},      -- left
+            l4 = {2008, 159,  2008, 1888}       -- right
+        }
+        
+        mapBattlefield = Map:new('battefield', '/pixel_art/scenario/battlefield.png', collision)
+    end
+    
+function drawPlayer()
+    player:draw()
+    playerStatus:drawLife(player.life, player.lifeDamaged, player.lifeEmpty, 75, camera.x, camera.y)
 end
